5098607037dd5e84cfc0219ddb55e35723e519b8
iTacHi-RS <mrxly12345@gmail.com>
2024-10-18 11:53:08 +0200

    gg

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
     public static void main(String []args){
-        System.out.println("ksiuyhsedgfg");
+        System.out.println("we might get fucked by java");
         
     }
 }
