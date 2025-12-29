5574dffd80e260447e92f21396fc54a4b05e72fd
Abdulrhman Mohammed <abdobusiness1999@gmail.com>
2025-12-30 00:22:42 +0200

    remove java

--- bazzaa/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
-
-Route::get('/', [StudentController::class, 'create']);
-
-Route::resource('students', StudentController::class);
